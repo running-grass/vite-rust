941f2c8d43694c2ba971be79096607302dd92727
Leo Liu <467195537@qq.com>
2022-10-23 18:08:33 +0800

    [ feat ] 引入vue3

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import wasmPack from 'vite-plugin-wasm-pack';
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   build: {
     minify: false
   },
-  plugins: [wasmPack(['./my-crate'])]
+  plugins: [vue(), wasmPack(['./my-crate']),]
 });
